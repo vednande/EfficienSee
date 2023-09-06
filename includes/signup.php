8bc009a686daf54f12e0c9f15256da04c315e524
45Piyush <singh45piyush@gmail.com>
2023-09-06 21:12:17 +0530

    Revert "Merge branch 'master' of https://github.com/vednande/EfficienSee"
    This reverts commit 58fd683357b7214ed3ee1928e01c47e2c57150f1, reversing
    changes made to 95c2c6a82243970fa2eeefe9ca399738f0120096.

--- includes/signup.php
+++ includes/signup.php
@@ -1,120 +1,4 @@
-<?php
-require_once "includes/config.php";
- 
-// Define variables and initialize with empty values
-$username=$firstname=$lastname=$address = $password = $confirm_password =$phonenumber=$email= "";
-$username_err = $password_err = $confirm_password_err = "";
- 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-	   
-	$firstname = trim($_POST["firstname"]);
-	$lastname = trim($_POST["lastname"]);
-	$email = trim($_POST["email"]);
-	$address = trim($_POST["address"]);
-	$phonenumber = trim($_POST["phonenumber"]);
-
- 
-    // Validate username
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter a username.";
-    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
-        $username_err = "Username can only contain letters, numbers, and underscores.";
-    } else{
-        // Prepare a select statement
-        $sql = "SELECT id FROM users WHERE UserName = ?";
-        
-        if($stmt = $mysqli->prepare($sql)){
-            // Bind variables to the prepared statement as parameters
-            $stmt->bind_param("s", $param_username);
-            
-            // Set parameters
-            $param_username = trim($_POST["username"]);
-            
-            // Attempt to execute the prepared statement
-            if($stmt->execute()){
-                // store result
-                $stmt->store_result();
-                
-                if($stmt->num_rows == 1){
-                    $username_err = "This username is already taken.";
-                } else{
-                    $username = trim($_POST["username"]);
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            $stmt->close();
-        }
-    }
-    
-    // Validate password
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";     
-    } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have atleast 6 characters.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-    
-    // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";     
-    } else{
-        $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($password_err) && ($password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
-        }
-    }
-    
-    // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-        
-        // Prepare an insert statement
-        $sql = "INSERT INTO users (FirstName,LastName,UserName,Email, Password,Phone,Address) VALUES (?, ?,?,?,?,?,?)";
-         
-        if($stmt = $mysqli->prepare($sql)){
-            // Bind variables to the prepared statement as parameters
-            $stmt->bind_param("ss", $param_username, $param_password,$param_lastname,$param_firstname,$param_email,$param_phone,$param_address);
-            
-            // Set parameters
-			$param_firstname = $firstname;
-			$param_lastname = $lastname;
-			$param_email = $email;
-			$param_address = $address;
-			$param_phone = $phonenumber;
-            $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-            
-            // Attempt to execute the prepared statement
-            if($stmt->execute()){
-                // Redirect to login page
-                header("location: login.php");
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            $stmt->close();
-        }
-    }
-    
-    // Close connection
-    $mysqli->close();
-}
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="assets/css/signup.css">
-
-	<title>Document</title>
-</head>
-<body>
+<link rel="stylesheet" href="assets/css/signup.css">
 <div class="signup">
     <div class="container">
         <div class="left">
@@ -152,7 +36,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 								</div>
 								<div class="form-group">
 									<label>Phone Number</label> 
-									<input class="form-control" type="number" name="phonenumber" id="phonenumber" >
+									<input class="form-control" type="number" name="phonenumber" id="phonenumber" pattern="[0-9]" required max="10" maxlength="10">
 									<!-- <input class="form-control" name="phonenumber" required type="ph"> -->
 								</div>
 								<div class="form-group">
@@ -160,33 +44,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 									<input class="form-control" type="text" name="address" id="address" required>
 								</div>
 								<div class="form-group">
-									<div class="row">
-										<div class="col">
-											<label style="margin-bottom: 5px; ">Confirm Password</label>
-										</div>
-									</div>
-									<input class="form-control" name="confirm password" required type="password">
-								
-								<!-- <div class="form-group">
 									<label>Upload Photo</label> 
 									<input type="file" accept="image/*" class="form-control input-file" name="photo" id="photo" style="height: auto; width: fit-content; background-color: transparent; border: 1px solid transparent;">
-								</div> -->
-								</div>
-								<div class="form-group" id="select-role"> 
-									<p>Please select your role</p>
-									<div class="hr" id="hr">
-										<input type="radio" name="role" id="hr" style="cursor: pointer;">
-										<label for="hr">HR</label>
-									</div>
-									<div class="employee" id="employee">
-										<input type="radio" name="role" id="employee" style="cursor: pointer;">
-										<label for="employee" style="text-transform: uppercase;">Employee</label>
-									</div>
 								</div>
 								<!-- <?php if($wrongpassword){echo $wrongpassword;}?> --> 
 								
 								<div class="form-group text-center">
-									<button class="btn btn-primary account-btn" name="signup" type="submit">sign up</button>
+									<button class="btn btn-primary account-btn" name="login" type="submit">sign up</button>
 										<div class="col-auto pt-2 forgot-password">
 											<a class="text-muted float-right"  href="forgot-password.php">
 												Forgot password?
@@ -203,7 +67,3 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         </div>
     </div>
 </div>
-	
-</body>
-</html>
-<script src="/project/efficiensee/includes/signup.js"></script>
