4a662442784b6fb4f45c601cc376eb88292c8d98
vednande <vednande10@gmail.com>
2023-09-30 20:32:53 +0530

    user landing base logic done

--- dynChange/userLanding.js
+++ dynChange/userLanding.js
@@ -6,20 +6,39 @@
     profit = 55
 */
 console.log("User landing page!");
-const earningsDyn = document.getElementById("earnings-dynamic");
-const expensesDyn = document.getElementById("expenses-dynamic");
-const profitDyn = document.getElementById("profit-dynamic");
 
-let earnings = 100;
-let expenses = 45;
-let profit = earnings - expenses;
-const min = -10;
-const max = 10;
-let percentChange;
+const changeNumberDynamically = () => {
+  const earningsDyn = document.getElementById("earnings-dynamic");
+  const expensesDyn = document.getElementById("expenses-dynamic");
+  const profitDyn = document.getElementById("profit-dynamic");
+  let earnings = 100;
+  let expenses = 45;
+  let profit = earnings - expenses;
+  const min = -10;
+  const max = 10;
+  let percentChange;
 
-setTimeout(() => {
   percentChange = Math.random() * (max - min) + min;
   earnings += percentChange;
   profit += earnings - expenses;
-  earningsDyn.innerText = earnings;
-}, 2000);
+  earningsDyn.innerText = earnings.toFixed(2);
+  expensesDyn.innerText = expenses.toFixed(2);
+  profitDyn.innerText = profit.toFixed(2);
+};
+
+setInterval(changeNumberDynamically, 3000);
+
+// setTimeout(() => {
+//   percentChange = Math.random() * (max - min) + min;
+//   earnings += percentChange;
+//   profit += earnings - expenses;
+//   earningsDyn.innerText = earnings;
+// }, 2000);
+
+// function changeNumberDynamic() {
+//   const earningsDyn = document.getElementById("earnings-dynamic");
+//   const newContent = "New Content " + new Date().toLocaleTimeString();
+//   earningsDyn.textContent = newContent;
+// }
+
+// setInterval(changeNumberDynamic, 3000);
